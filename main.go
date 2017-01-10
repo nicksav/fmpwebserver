98b3b50a444739d8db7d53efbb42597f35181ed0
nicksav <inci@dezcom.org>
2017-01-10 17:10:17 +1000

    Changed to run

--- main.go
+++ main.go
@@ -3,14 +3,15 @@ package main
 import (  
     "fmpwebserver/routers"
     "fmpwebserver/settings"
-    "fmpwebserver/services/models"
     "github.com/urfave/negroni"
     "net/http"
 )
 
+//import  "fmpwebserver/services/models"
+
 func main() {  
     settings.Init()
-    models.InitDB()
+    //models.InitDB()
     router := routers.InitRoutes()
     n := negroni.Classic()
     n.UseHandler(router)
